8b5f1cbd506000c95735e87f147fe4151aba5ecb
Taras <tarvar988@gmail.com>
2023-01-18 18:22:49 +0200

    Passwords now are stored in environmental variables

--- send_email.py
+++ send_email.py
@@ -1,11 +1,12 @@
 import smtplib, ssl
+import os
 
 
 def send_email(message):
     host = "smtp.gmail.com"
     port = 465
     username = "wizardtaras@gmail.com"
-    password = "xcpyrhkvkakwkvya"
+    password = os.getenv('Portfolio_PASSWORD')
     receiver = "wizardtaras@gmail.com"
     context = ssl.create_default_context()
 
