6be6aef3f3ddafb9504f2ed8cdda323d51b02852
Taras <tarvar988@gmail.com>
2023-01-18 16:21:54 +0200

    Send email feature (completed)

--- send_email.py
+++ send_email.py
@@ -1,20 +1,14 @@
-import smtplib
-import ssl
+import smtplib, ssl
 
-host = "smtp.gmail.com"
-port = 465
 
-username = "wizardtaras@gmail.com"
-password = "xcpyrhkvkakwkvya"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "wizardtaras@gmail.com"
+    password = "xcpyrhkvkakwkvya"
+    receiver = "wizardtaras@gmail.com"
+    context = ssl.create_default_context()
 
-receiver = "wizardtaras@gmail.com"
-context = ssl.create_default_context()
-
-message = """\
-Subject: Nothing
-Hi! How are you? Bye!
-"""
-
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    server.sendmail(username, receiver, message)
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
